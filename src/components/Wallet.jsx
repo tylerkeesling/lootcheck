47bcce1cc856990b823e384870eaae5543bb55cf
Keesling <keeslity@vertafore.com>
2018-06-15 09:12:11 -0600

    added deposit functionality

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { deposit } from '../actions/balance';
 
 export class Wallet extends React.Component {
   state = { balance: undefined };
 
   updateBalance = e => {
     this.setState({ balance: parseInt(e.target.value, 10) });
-  };
+	};
+	
+	deposit = () => {
+		console.log('deposit was clicked');
+		this.props.deposit(this.state.balance)
+	};
 
   render() {
     return (
@@ -14,6 +20,7 @@ export class Wallet extends React.Component {
         <h3 className="balance">Wallet balance: {this.props.balance}</h3>
         <br />
         <input className="input-wallet" onChange={this.updateBalance} />
+				<button className='btn-deposit' onClick={this.deposit}>Deposit</button>
       </div>
     );
   }
@@ -25,7 +32,15 @@ function mapStateToProps(state) {
   };
 }
 
+function mapDispatchToProps(dispatch) {
+	return {
+		deposit: (balance) => {
+			dispatch(deposit(balance))
+		}
+	}
+}
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(Wallet);
--- src/components/__test__/Wallet.test.js
+++ src/components/__test__/Wallet.test.js
@@ -3,7 +3,8 @@ import { shallow } from 'enzyme';
 import { Wallet } from '../Wallet';
 
 describe('Wallet', () => {
-	const props = { balance: 20 };
+	const mockDeposit= jest.fn();
+	const props = { balance: 20, deposit: mockDeposit };
 	const wallet = shallow(<Wallet {...props} />);
 
 	it('renders', () => {
@@ -27,6 +28,14 @@ describe('Wallet', () => {
 
 		it('updates the local wallet balance in the `state` and converts it to a number', () => {
 			expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+		});
+
+		describe('and the user wants to make a deposit', () => {
+			beforeEach(() => wallet.find('.btn-deposit').simulate('click'));
+
+			it('dispatchs the `deposit()` it recieves from props with local balance', () => {
+				expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
+			});
 		});
 	});
 });
