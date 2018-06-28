8a18d131ac7fa4c81283107a6f781bf47d1cb326
Tyler Keesling <tpkeesling@gmail.com>
2018-06-28 09:00:01 -0600

    fixed a tag node find in test

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -18,6 +18,6 @@ describe('App', () => {
 	});
 
 	it('contains a link to the Coindesk price page', () => {
-		expect(app.find(a).props().href).toBe('https://www.coindesk.com/price');
+		expect(app.find('a').props().href).toBe('https://www.coindesk.com/price');
 	});
 });
